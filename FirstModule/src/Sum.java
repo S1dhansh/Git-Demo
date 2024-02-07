3f71b08a9867d440bc00915ca479ded83b0ebe44
S1dhansh <sidhanshsharma0@gmail.com>
2024-02-07 22:26:05 +0530

    just giving a try

--- FirstModule/src/Sum.java
+++ FirstModule/src/Sum.java
@@ -1,8 +1,8 @@
 public class Sum {
     public static void main(String[] args) {
         int a,b,sum;
-        a=5;
-        b=6;
+        a=6;
+        b=7;
         sum=a+b;
         System.out.println("The sum is " + sum);
 

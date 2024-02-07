5b21dc6eacd4af32541e400d5d8583fa350c5ce1
S1dhansh <sidhanshsharma0@gmail.com>
2024-02-07 22:28:54 +0530

    This is commit try

--- FirstModule/src/Sum.java
+++ FirstModule/src/Sum.java
@@ -1,8 +1,8 @@
 public class Sum {
     public static void main(String[] args) {
         int a,b,sum;
-        a=5;
-        b=6;
+        a=1;
+        b=2;
         sum=a+b;
         System.out.println("The sum is " + sum);
 
